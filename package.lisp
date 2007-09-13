b4aa7c0a89e7d8d357d7d50fe5e6a008b69a13d6
Marcus Pearce <m.pearce@gold.ac.uk>
2007-09-13 10:29:29 +0000

    package.lisp: export +UNDEFINED+

--- package.lisp
+++ package.lisp
@@ -1,6 +1,6 @@
 (cl:defpackage #:viewpoints 
   (:use #:common-lisp)
-  (:export "UNDEFINED-P" "GET-VIEWPOINT" "GET-VIEWPOINTS" 
+  (:export "+UNDEFINED+" "UNDEFINED-P" "GET-VIEWPOINT" "GET-VIEWPOINTS" 
            "VIEWPOINT-ELEMENT" "VIEWPOINT-SEQUENCE" "VIEWPOINT-SEQUENCES"
            "VIEWPOINT-ALPHABET" "VIEWPOINT-TYPESET" "VIEWPOINT-LINKS")
   (:documentation "Multiple viewpoint framework for music representation."))
