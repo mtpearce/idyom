c1f54e021f1ff2a25c873005a2d980b29791c9dd
Bastiaan van der Weij <bjvanderweij@gmail.com>
2015-12-14 12:24:19 +0000

    Fix metrical onset accent:use phase and correct time units.

--- viewpoints/melody/metrical.lisp
+++ viewpoints/melody/metrical.lisp
@@ -1,6 +1,19 @@
 (cl:in-package #:viewpoints)
 
-;; Metrical accent
+; A derived viewpoint that calculates metrical position under an interpretation as a proportion of the time signature's period
+(define-metrical-viewpoint (metrical-onset-position metrical (onset))
+    ((events md:melodic-sequence)
+     (interpretation md:metrical-interpretation) element)
+  :function (let ((period (md:meter-period interpretation))
+		  (phase (md:meter-phase interpretation))
+		  (timebase (md:timebase (last-element events)))
+		  (resolution (md:resolution interpretation))
+		  (onset (onset events)))
+	      (let ((phase-corrected-onset
+		     (- onset (* phase (/ timebase resolution))))
+		    (converted-period (* period (/ timebase resolution))))
+		(/ (mod phase-corrected-onset converted-period) converted-period))))
+
 (define-metrical-viewpoint (metrical-onset-accent metrical (onset))
     ((events md:melodic-sequence) 
      (interpretation md:metrical-interpretation) element)
@@ -8,7 +21,12 @@
               (if (null event) +undefined+
                   (let ((pulses (md:pulses interpretation))
 			(barlength (md:barlength interpretation))
-                        (onset (onset events))
-			(timebase (md:timebase (last-element events))))
-		    (+ (metrical-accent-multiple onset pulses barlength timebase)
-			   (metrical-accent-division onset pulses barlength))))))
+			(phase (md:meter-phase interpretation))
+			(onset (onset event))
+			(resolution (md:resolution interpretation))
+			(timebase (md:timebase interpretation)))
+		    (let ((phase-corrected-onset
+			   (- onset (* phase (/ timebase resolution)))))
+		      (+ (metrical-accent-multiple 
+			  phase-corrected-onset pulses barlength timebase)
+			 (metrical-accent-division phase-corrected-onset pulses barlength)))))))
