7a7ba06a93c1a4a1aed0d0381d89c4d89965daa4
Marcus Pearce <marcus.pearce@qmul.ac.uk>
2016-05-17 17:59:57 +0100

    Update utils/package.lisp

--- utils/package.lisp
+++ utils/package.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       package.lisp
 ;;;; Author:     Marcus Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2003-04-05 18:54:17 marcusp>                           
-;;;; Time-stamp: <2016-04-25 11:07:18 marcusp>                           
+;;;; Time-stamp: <2016-05-17 17:59:13 marcusp>                           
 ;;;; ======================================================================
 
 (cl:in-package #:cl-user)
@@ -11,6 +11,7 @@
   (:use #:cl)
   (:export "ROUND-TO-NEAREST-DECIMAL-PLACE" "AVERAGE" "GENERATE-INTEGERS"
 	   "POWERSET" "QUOTIENT" "FACTORIAL" "N-PERMUTATIONS" "N-COMBINATIONS"
+	   "RANGE" "CUMSUM" "MD5-SUM-OF-LIST"
            "NTH-ROOT" "STRING-APPEND" "SPLIT-STRING" 
            "INSERTION-SORT" "CARTESIAN-PRODUCT" "FLATTEN" "COMBINATIONS"
 	   "FLATTEN-ORDER" "COUNT-FREQUENCIES" "NUMERIC-FREQUENCIES"
@@ -23,5 +24,17 @@
            "CD" "PWD" "ENSURE-DIRECTORY"
            "COLLECT-GARBAGE" "SHELL-COMMAND")
   (:documentation "Utility functions of general use."))
+
+(defpackage #:python
+  (:use #:cl)
+  (:export "ALIST->DICT" "PLIST->DICT" "LIST->LIST")
+  (:documentation "Utility functions for exporting python code."))
+
+(defpackage #:promises
+  (:use #:cl)
+  (:export "MAKE-PROMISE" "PROMISE" "RETRIEVE" "GET-IDENTIFIER")
+  (:documentation "Objects for postponing calling resource intensive functions until
+their result is required because of cache misses."))
+
 
 
