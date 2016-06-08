bc2659391d03bdd74e6ef0fb6bc7ae2ea398ff57
Bastiaan van der Weij <bjvanderweij@gmail.com>
2016-06-08 22:05:15 +0200

    Move metrical position and accent calculation out of viewpoint definition.

--- viewpoints/melody/metrical.lisp
+++ viewpoints/melody/metrical.lisp
@@ -1,32 +1,39 @@
 (cl:in-package #:viewpoints)
 
+(defun calculate-metrical-position (onset interpretation)
+  (let ((barlength (md:barlength interpretation))
+	(timebase (md:timebase interpretation))
+	(phase (md:meter-phase interpretation)) ;; Confusing: phase is specified in grid-points
+	(resolution (md:resolution interpretation)))
+    (let ((phase-corrected-onset
+	   (- onset (* phase (/ timebase resolution)))))
+      ;;      (/ (mod phase-corrected-onset barlength) barlength))))
+      (mod phase-corrected-onset barlength))))
+
 ; A derived viewpoint that calculates metrical position under an interpretation as a proportion of the time signature's period
 (define-metrical-viewpoint (metrical-onset-position metrical (onset))
     ((events md:melodic-sequence)
      (interpretation md:metrical-interpretation) element)
-  :function (let ((period (md:meter-period interpretation))
-		  (phase (md:meter-phase interpretation))
-		  (timebase (md:timebase (last-element events)))
-		  (resolution (md:resolution interpretation))
-		  (onset (onset events)))
-	      (let ((phase-corrected-onset
-		     (- onset (* phase (/ timebase resolution))))
-		    (converted-period (* period (/ timebase resolution))))
-		(/ (mod phase-corrected-onset converted-period) converted-period))))
+  :function (let ((onset (onset events)))
+	      (calculate-metrical-position onset interpretation)))
+
+(defun calculate-metrical-accent (onset interpretation)
+    (let ((pulses (md:pulses interpretation))
+	  (barlength (md:barlength interpretation))
+	  (phase (md:meter-phase interpretation))
+	  (resolution (md:resolution interpretation))
+	  (timebase (md:timebase interpretation)))
+      (let ((phase-corrected-onset
+	     (- onset (* phase (/ timebase resolution)))))
+	(+ (metrical-accent-multiple 
+	    phase-corrected-onset pulses barlength timebase)
+	   (metrical-accent-division phase-corrected-onset pulses barlength)))))
 
 (define-metrical-viewpoint (metrical-onset-accent metrical (onset))
     ((events md:melodic-sequence) 
      (interpretation md:metrical-interpretation) element)
-  :function (let ((event (last events)))
+  :function (let ((event (last events))
+		  (onset (onset events)))
               (if (null event) +undefined+
-                  (let ((pulses (md:pulses interpretation))
-			(barlength (md:barlength interpretation))
-			(phase (md:meter-phase interpretation))
-			(onset (onset event))
-			(resolution (md:resolution interpretation))
-			(timebase (md:timebase interpretation)))
-		    (let ((phase-corrected-onset
-			   (- onset (* phase (/ timebase resolution)))))
-		      (+ (metrical-accent-multiple 
-			  phase-corrected-onset pulses barlength timebase)
-			 (metrical-accent-division phase-corrected-onset pulses barlength)))))))
+		  (calculate-metrical-accent onset interpretation))))
+
