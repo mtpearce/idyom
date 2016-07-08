bad3d2c05683fe04efa1e8208812abdb0c2f45fe
Bastiaan van der Weij <bjvanderweij@gmail.com>
2016-07-08 16:51:24 +0200

    Some refactoring in latex code.

--- apps/latex.lisp
+++ apps/latex.lisp
@@ -14,14 +14,14 @@
     (format nil "\\begin{tabular}{~{~D~}}~%~A~A~A\\end{tabular}" 
 	    (loop for x to (length events) collect "l")
 	    (if enumerate (enumeration-row (length events)) "")
-	    (if (eql texture :grid) (generate-grid-row events) "")
-	    (generate-table-rows events viewpoints 
-				 :interpretations interpretations))))
+	    (if (eql texture :grid) (grid-row events) "")
+	    (viewpoint-rows events viewpoints 
+			    :interpretations interpretations))))
 
 (defun enumeration-row (n)
   (format nil "event index & ~{~A ~^& ~}\\\\~%" (utils:generate-integers 0 (1- n))))
 
-(defun generate-grid-row (grid-events)
+(defun grid-row (grid-events)
   (let ((is-onsets 
 	 (viewpoints:viewpoint-sequence (viewpoints:get-viewpoint 'is-onset) grid-events)))
     (flet ((draw-grid-point (is-onset)
@@ -42,14 +42,16 @@
 
 (defgeneric viewpoint-row (events viewpoint &key &allow-other-keys))
 (defmethod viewpoint-row (events (viewpoint viewpoints::metrical) &key interpretations)
-  (dolist (interpretation interpretations)
-    (format nil "\\texttt{~A ~D ~D ~D} & ~{~A ~^& ~}\\\\~%"
-	    (viewpoints:viewpoint-name viewpoint)
-	    (md:barlength interpretation)
-	    (md:pulses interpretation)
-	    (md:interpretation-phase interpretation)
-	    (mapcar #'viewpoint-element
-		    (viewpoint-elements viewpoint events :interpretation interpretation)))))
+  (format nil "~{~A~}"
+	  (loop for interpretation in interpretations collecting
+	       (format nil "\\texttt{~A ~D ~D ~D} & ~{~A ~^& ~}\\\\~%"
+		       (viewpoints:viewpoint-name viewpoint)
+		       (md:barlength interpretation)
+		       (md:pulses interpretation)
+		       (md:interpretation-phase interpretation)
+		       (mapcar #'viewpoint-element
+			       (viewpoint-elements viewpoint events
+						   :interpretation interpretation))))))
 
 (defmethod viewpoint-row (events (viewpoint viewpoints:viewpoint) &key &allow-other-keys)
     (format nil "\\texttt{~A} & ~{~A ~^& ~}\\\\~%"
@@ -57,18 +59,32 @@
 	    (mapcar #'viewpoint-element
 		    (viewpoint-elements viewpoint events))))
 
-(defun generate-table-rows (events viewpoints 
+(defun viewpoint-rows (events viewpoints 
 			    &key interpretations)
-  (flet ((draw-viewpoint-row (vp)
-	   (flet ((draw-viewpoint-element (element)
-		    (format nil "~D"
-			    (if (eql element viewpoints:+undefined+)
-				"$\\perp$"
-				element))))
-	     (format nil "\\texttt{~A} & ~{~A ~^& ~}\\\\~%"
-		     (viewpoints:viewpoint-name vp)
-		     (mapcar #'draw-viewpoint-element
-			     (viewpoint-elements vp events :interpretations interpretations))))))
-    (format nil "~{~A~}" (mapcar #'draw-viewpoint-row viewpoints))))
+  (format nil "~{~A~}" (mapcar #'(lambda (vp)
+				   (viewpoint-row events vp :interpretations interpretations))
+			       viewpoints)))
+
+(defun predictions-row (events viewpoint &key interpretation)
+    (let* ((sources (viewpoints:get-viewpoints sources))
+	 (targets (viewpoints:get-basic-viewpoints targets training-set texture))
+	 ;; Obtain event counts per category
+	 (category-counts (inference::count-categories training-set texture resolution :use-cache? nil))
+	 ;; Extract a list of metrical interpretations
+	 (categories (mapcar #'(lambda (category-count) 
+				 (md:category-string->metrical-interpretation 
+				  (car category-count)))
+			     category-counts))
+	 (models (inference::make-category-models training-set dataset-id
+						  categories targets sources
+						  :use-cache? t
+						  :texture texture
+						  :resolution resolution)))
+    (inference::generate-category-predictions categories models event-sequence
+					      :resolution resolution :texture texture
+					      :keep-prediction-sets t)))
+
+  
+			
 
 
