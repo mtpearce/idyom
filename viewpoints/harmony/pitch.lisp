d1595b93d58cfd4093ab91a25596f4739d302b19
Peter M C Harrison <pmc.harrison@gmail.com>
2017-03-06 11:34:02 +0000

    Adding viewpoint h-gct-horiz-root-interval

--- viewpoints/harmony/pitch.lisp
+++ viewpoints/harmony/pitch.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       pitch.lisp
 ;;;; Author:     Peter Harrison <p.m.c.harrison@qmul.ac.uk>
 ;;;; Created:    <2017-03-03 10:13:20 peter>                              
-;;;; Time-stamp: <2017-03-03 15:49:56 peter>                           
+;;;; Time-stamp: <2017-03-06 11:27:06 peter>                           
 ;;;; ======================================================================
 ;;;;
 ;;;; Description ==========================================================
@@ -36,5 +36,18 @@
 		  +undefined+ 
 	      (mapcar #'(lambda (x) (mod (- x local-tonic) 12))
 		      (h-cpitch-class-set events)))))
-		  
+
+(define-viewpoint (h-gct-horiz-root-interval derived (h-cpitch))
+    ;; Chromatic interval between the root of the current chord
+    ;; and the root of the previous chord. Returns +undefined+
+    ;; if the root of the previous chord is undefined.
+    ((events md:harmonic-sequence) element)
+  :function (multiple-value-bind (e1 e2)
+                (values-list (last events 2))
+              (if (or (null e1) (null e2)) +undefined+
+                  (let ((root1 (h-gct-root (list e1)))
+                        (root2 (h-gct-root (list e2))))
+                    (if (undefined-p root1 root2) +undefined+
+                        (mod (- root2 root1) 12))))))
+  
      
