22ecddebfe4becd3e20905ba0c1b7fe0edf150ff
Peter M C Harrison <pmc.harrison@gmail.com>
2017-03-28 17:53:17 +0100

    Added a function to create a sample harmonic sequence, for testing purposes

--- viewpoints/harmony/basic-viewpoints.lisp
+++ viewpoints/harmony/basic-viewpoints.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       basic-viewpoints.lisp
 ;;;; Author:     Marcus  Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2014-09-25 19:09:17 marcusp>                           
-;;;; Time-stamp: <2017-03-14 10:14:18 peter>                           
+;;;; Time-stamp: <2017-03-28 17:52:10 peter>                           
 ;;;; ======================================================================
 
 (cl:in-package #:viewpoints)
@@ -54,3 +54,68 @@
 		      (mapcar #'round pitches)
 		      pitches)))
     pitches))
+
+;;;==============================
+;;;* Testing functions *
+;;;==============================
+
+(defun make-harmonic-seq
+    (cpitch &key onset dur)
+  "Intended for testing purposes. Makes an object of class md:harmonic-sequence
+with attributes specified by <cpitch>, <onset>, and <dur>. <cpitch> should be
+a list of lists, with the ith element of the jth list corresponding to the
+ith chromatic pitch in the jth chord. By default, each chord
+has duration 100 and onset 100 basic time units after the onset of
+the previous chord. It is possible to specify <onset> manually, 
+in which case <onset> should be a list with the ith element
+being the onset of the ith chord. Chords should be listed in order
+of increasing onset. In this case the duration of each
+chord will default to the inter-onset interval between each chord,
+unless <dur> is specified manually, in which case <dur> should 
+be a list the ith element of which is the duration of the ith chord."
+  (if (or (null cpitch) (not (listp cpitch)))
+      (error "<cpitch> must be a non-empty list."))
+  (let* ((num-chords (length cpitch))
+	 (onset (cond ((null onset)
+		       (loop for x from 0 to (1- num-chords)
+			  collect (* 100 x)))
+		      ((eql (length onset) num-chords)
+		       onset)
+		      (t (error "<onset> must be NIL or a list of same length as <cpitch>."))))
+	 (dur (cond ((null dur)
+		     (if (eql (length onset) 1)
+			 '(100)
+			 (append (loop
+				    for a in (butlast onset)
+				    for b in (cdr onset)
+				    collect (- b a))
+				 '(100))))
+		    ((eql (length dur) num-chords) dur)
+		    (t (error "<dur> must be NIL or a list of same length as <cpitch>.")))))
+    (if (not (every #'(lambda (x) (> x 0)) dur))
+	(error "Every element of <dur> must be greater than 0."))
+    (let* ((seq (make-instance 'md:harmonic-sequence
+			       :onset 0
+			       :duration (+ (car (last onset)) (car (last dur)))
+			       :midc 60))
+	   (slices nil))
+      (loop
+	 for chord in cpitch
+	 for o in onset
+	 for d in dur
+	 do (progn
+	      (let ((events (mapcar #'(lambda (pitch)
+					(let ((event (make-instance 'md::music-event
+								    :cpitch pitch)))
+					  (md:set-attribute event 'onset o)
+					  (md:set-attribute event 'dur d)
+					  event))
+				    chord))
+		    (slice (make-instance 'md:music-slice
+					  :onset o :duration d)))
+		(sequence:adjust-sequence slice (length events)
+					  :initial-contents events)
+		(push slice slices))))
+      (sequence:adjust-sequence seq (length slices)
+				:initial-contents (reverse slices))
+      seq)))
