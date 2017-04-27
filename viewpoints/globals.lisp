2fd6a060e68d321afe1125076e0721159f0f9383
Peter M C Harrison <pmc.harrison@gmail.com>
2017-04-27 17:07:32 +0100

    Adding facility to estimate viewpoint quantiles based on given datasets

--- viewpoints/globals.lisp
+++ viewpoints/globals.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       globals.lisp
 ;;;; Author:     Peter Harrison <p.m.c.harrison@qmul.ac.uk>
 ;;;; Created:    <2017-04-27 12:36:01 peter>                           
-;;;; Time-stamp: <2017-04-27 12:39:48 peter>                           
+;;;; Time-stamp: <2017-04-27 17:04:14 peter>                           
 ;;;; ======================================================================
 
 ;;;; This file contains global variables for the viewpoints package.
@@ -12,6 +12,6 @@
 (defconstant +undefined+ '@ "The undefined symbol.")
 (defvar *basic-types* nil) ;;(make-hash-table))
 
-(defparameter *viewpoint-quantiles* (make-hash-table))
+(defparameter *viewpoint-quantiles* nil)
 
 
