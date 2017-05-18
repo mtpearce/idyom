2d0f11b1b65f21592cdd167719685958c0966004
Peter M C Harrison <pmc.harrison@gmail.com>
2017-05-18 18:49:36 +0100

    Fixing treatment of rounding continuous viewpoints for undefined symbols

--- viewpoints/macros.lisp
+++ viewpoints/macros.lisp
@@ -23,8 +23,11 @@ is a continuous viewpoint or not."
        (defmethod ,name ((,events list))
 	 (declare (ignorable ,events))
 	 ,(if continuous
-	      `(let ((continuous-output
-		      (utils:round-to-nearest-decimal-place ,function 10)))
+	      `(let* ((continuous-output ,function)
+		      (continuous-output
+		       (if (undefined-p continuous-output)
+			   continuous-output
+			   (utils:round-to-nearest-decimal-place ,function 10))))
 		 (if *discretise-viewpoints*
 		     (let ((quantiles (gethash (symbol-name ',name)
 					       *viewpoint-quantiles*)))
