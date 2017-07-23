4747138e77c80af145d585efa3e5fbe5f1ecd3da
Peter M C Harrison <pmc.harrison@gmail.com>
2017-07-23 12:42:28 +0100

    Defining generic function for counting n-grams

--- apps/descriptives.lisp
+++ apps/descriptives.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       descriptives.lisp
 ;;;; Author:     Peter Harrison <p.m.c.harrison@qmul.ac.uk>
 ;;;; Created:    <2017-07-23 12:30:38 peter>                          
-;;;; Time-stamp: <2017-07-23 12:31:17 peter>                           
+;;;; Time-stamp: <2017-07-23 12:42:09 peter>                           
 ;;;; ======================================================================
 ;;;;
 ;;;; DESCRIPTION 
@@ -14,3 +14,11 @@
 
 (cl:in-package #:descriptives)
 
+(defgeneric count-n-grams (data n &key output-csv overwrite-csv)
+  (:documentation "Counts <n>-grams in <data>. If <output-csv> is provided
+then the output is saved as a csv file to path <output-csv>, as long as 
+either no object exists at that location or <overwrite-csv> is not null.
+The n-grams are returned as an EQL hash table where the keys are lists, each
+list being a list of viewpoint elements corresponding to an n-gram, and the 
+values are integer counts."))
+  
