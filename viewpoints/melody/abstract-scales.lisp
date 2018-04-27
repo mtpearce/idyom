8dd26b2529e9e179002ec9587abde98ee259af09
Bastiaan van der Weij <bjvanderweij@gmail.com>
2018-04-27 23:16:47 +0200

    fix the abs-sdeg-west-2 viewpoint following theoretical insight

--- viewpoints/melody/abstract-scales.lisp
+++ viewpoints/melody/abstract-scales.lisp
@@ -28,7 +28,15 @@
 			     (if (< interval 0) 1 0))
 			  7))))))
 
-(define-viewpoint (sdeg-west-2 derived (cpitch))
+(define-abstract-viewpoint (abs-sdeg-west-2 (cpitch) (:keysig :mode) () abs-sdeg-west-2-training)
+    ((events md:melodic-sequence) element) 
+  :function (lambda (keysig mode)
+	      (let* ((pitch (cpitch events))
+		     (interval (get-tonint pitch (octave (list (car events)))
+					   keysig mode)))
+		(mod interval *octave*))))
+
+(define-viewpoint (sdeg-west-3 derived (cpitch))
     ((events md:melodic-sequence) element) 
   :function (let* ((interval (tonint events))
 		   (scale-intervals (case (mode events)
@@ -39,7 +47,7 @@
 		  (- (mod interval *octave*))
 		  degree)))
 
-(define-abstract-viewpoint (abs-sdeg-west-2 (cpitch) (:keysig :mode) () abs-sdeg-west-training-2)
+(define-abstract-viewpoint (abs-sdeg-west-3 (cpitch) (:keysig :mode) () abs-sdeg-west-3-training)
     ((events md:melodic-sequence) element) 
   :function (lambda (keysig mode)
 	      (let* ((pitch (cpitch events))
