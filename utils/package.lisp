f37d387841e15368d2f32db0d0d5d94d518689c4
Marcus Pearce <marcus.pearce@qmul.ac.uk>
2018-08-13 22:44:41 +0200

    Move list->string to utils package.

--- utils/package.lisp
+++ utils/package.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       package.lisp
 ;;;; Author:     Marcus Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2003-04-05 18:54:17 marcusp>                           
-;;;; Time-stamp: <2018-07-02 13:47:52 marcusp>                           
+;;;; Time-stamp: <2018-08-10 09:47:01 marcusp>                           
 ;;;; ======================================================================
 
 (cl:in-package #:cl-user)
@@ -12,11 +12,12 @@
   (:export "ROUND-TO-NEAREST-DECIMAL-PLACE" "AVERAGE" "GENERATE-INTEGERS"
 	   "POWERSET" "QUOTIENT" "FACTORIAL" "N-PERMUTATIONS" "N-COMBINATIONS"
 	   "RANGE" "CUMSUM" "MD5-SUM-OF-LIST" "SHUFFLE"
-           "NTH-ROOT" "STRING-APPEND" "SPLIT-STRING" "ANY-DUPLICATED"
+           "NTH-ROOT" "ANY-DUPLICATED"
            "INSERTION-SORT" "CARTESIAN-PRODUCT" "FLATTEN" "COMBINATIONS"
 	   "FLATTEN-ORDER" "COUNT-FREQUENCIES" "NUMERIC-FREQUENCIES"
            "FIND-DUPLICATES" "ROTATE" "PERMUTATIONS" "REMOVE-BY-POSITION"
            "RANDOM-SELECT"
+           "LIST->STRING" "STRING-APPEND" "SPLIT-STRING"
            "COPY-INSTANCE" "COPY-SLOT-VALUES" "INITIALISE-UNBOUND-SLOTS"
            "LAST-ELEMENT" "PENULTIMATE-ELEMENT" "LAST-N" "BUTLAST-N"
 	   "NMAPCAR" "NPOSITION" "NPOSITIONS" "NMEMBER" "NMIN" "NSELECTFIRST"
--- utils/utils.lisp
+++ utils/utils.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       utils.lisp
 ;;;; Author:     Marcus  Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2003-04-16 16:59:20 marcusp>
-;;;; Time-stamp: <2018-06-22 10:15:01 marcusp>
+;;;; Time-stamp: <2018-08-10 09:45:57 marcusp>
 ;;;; ======================================================================
 
 (cl:in-package #:utils)
@@ -105,6 +105,14 @@ Borrowed from https://www.pvk.ca/Blog/Lisp/trivial_uniform_shuffling.html"
 ;;;===========================================================================
 ;;; Strings 
 ;;;===========================================================================
+
+(defun list->string (list)
+  (reduce #'(lambda (&optional s1 s2) (utils:string-append s1 s2))
+          (mapcar #'(lambda (e)
+                      (format nil "~A " (if (symbolp e)
+                                            (symbol-name e)
+                                            e)))
+                  list)))
 
 (defun string-append (&rest args)
   "Concatenates its string arguments <args>."
