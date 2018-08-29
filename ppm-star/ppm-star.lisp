9ef7b53da2a3b9799a4c9566c1b08ee620a668ac
Marcus Pearce <marcus.pearce@qmul.ac.uk>
2018-08-29 13:56:29 +0200

    Don't increment counts for branch locations.

--- ppm-star/ppm-star.lisp
+++ ppm-star/ppm-star.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       ppm-star.lisp
 ;;;; Author:     Marcus Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2002-07-02 18:54:17 marcusp>                           
-;;;; Time-stamp: <2018-08-29 13:04:57 marcusp>                           
+;;;; Time-stamp: <2018-08-29 13:46:45 marcusp>                           
 ;;;; ======================================================================
 ;;;;
 ;;;; DESCRIPTION 
@@ -769,7 +769,8 @@ tree, otherwise it is just the label of the corresponding node."
   (labels ((increment-count (location update-excluded)
              ;; (format t "~%INCREMENT-COUNT ~A ~A" (location->string location) update-excluded)
              (if (branch-p location)
-                 (unless (retrieve-virtual-node m location)
+                 (unless (gethash location (ppm-virtual-nodes m))
+                   ;; (retrieve-virtual-node m location)
                    ;; (format t "~%Branch ~A: ~A ~A~&" (instantiate-label m (get-label m location))
                    ;;        (get-count m location nil) (get-count m location t))
                    (increment-node-record-count (get-record m location) update-excluded))
