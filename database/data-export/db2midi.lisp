673fe31aee4b024f3cb3f07e4ec8aa6d95b3fb3e
Marcus Pearce <marcus.pearce@qmul.ac.uk>
2020-02-05 10:59:02 +0000

    Allow midi export for lists of md:music-event objects.

--- database/data-export/db2midi.lisp
+++ database/data-export/db2midi.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       db2midi.lisp
 ;;;; Author:     Marcus Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2005-06-09 11:01:51 marcusp>
-;;;; Time-stamp: <2015-03-25 16:33:47 marcusp>
+;;;; Time-stamp: <2020-02-05 10:56:00 marcusp>
 ;;;; ======================================================================
 
 (cl:in-package #:db2midi)
@@ -15,32 +15,94 @@
 (defvar *default-velocity* 100)
 (defvar *default-program* 0)
 
+;;; Export data
+
 (defmethod export-data ((d idyom-db:mtp-dataset) (type (eql :mid)) path)
-  (let ((*timebase* (idyom-db::dataset-timebase d))
-        (*midc*     (idyom-db::dataset-midc d)))
-    (dolist (c (idyom-db::dataset-compositions d))
+  "Export an entire dataset from the database." 
+  (let* ((id (car (idyom-db:get-id d)))
+         (*timebase* (idyom-db:get-timebase id))
+         (*midc*     (idyom-db:get-midc id)))
+    (dolist (c (idyom-db:get-compositions id))
       (export-data c type path))))
 
 (defmethod export-data ((c idyom-db:mtp-composition) (type (eql :mid)) path)
-  ;; FIXME: *midc* is never set if export-data is called with a
-  ;; composition directly.
-  (let* ((title (idyom-db::composition-description c))
+  "Export an individual composition from the database."
+  (let* ((id (idyom-db:get-id c))
+         (title (idyom-db:get-description (first id) (second id)))
          (file (concatenate 'string path "/" title ".mid"))
-         (*timebase* (idyom-db::composition-timebase c)))
-    (events->midi (idyom-db::composition-events c) file)))
+         (*timebase* (idyom-db:get-timebase (first id) (second id)))
+         (*midc* (idyom-db:get-midc (first id))))
+    (db-events->midi (idyom-db:composition-events c) file)))
 
 (defmethod export-data ((event-list list) (type (eql :mid)) path)
-  (let* ((first-event (car event-list))
-	 (first-id (identifier first-event))
-         (title (idyom-db:get-description (dataset-id first-id)
-					   (composition-id first-id)))
-         (file (concatenate 'string path "/" title ".mid")))
-    (events->midi event-list file)))
+  "Export a list of events, which may be database events (mtp-event)
+or music objects events (music-event)."
+  (let ((first-event (car event-list)))
+    (typecase first-event
+      (idyom-db:mtp-event
+       (let* ((first-id (idyom-db:get-id first-event))
+              (title (idyom-db:get-description (first first-id) (second first-id)))
+              (file (concatenate 'string path "/" title ".mid")))
+         (db-events->midi event-list file)))
+      (md:music-event
+       (let* ((id (md:get-identifier first-event))
+              (did (md:get-dataset-index id))
+              (cid (md:get-composition-index id))
+              (description (idyom-db:get-description did cid))
+              (filename (concatenate 'string path "/" description ".mid")))
+         (print filename)
+         (mo-events->midi event-list filename))))))
 
-(defun bpm->usecs (bpm)
-  (floor (* 1000000 (/ 60 bpm))))
 
-(defun event-lists->midi (event-lists file &key (format 1) (program *default-program*))
+;;; Music Objects
+
+(defun mo-events->midi (events file &key (format 1) (program *default-program*))
+  "Writes a list of database events <events> to a midi file <file>."
+  (let* ((channel (md:get-attribute (car events) :voice))
+         (channel-msg (make-instance 'midi:program-change-message :time 0 
+                                     :status (+ #xc0 channel) 
+                                     :program program))
+         (tempo (md:get-attribute (car events) :tempo))
+         (tempo-msg (make-instance 'midi:tempo-message :time 0
+                                   :status #xff
+                                   :tempo (bpm->usecs 
+                                           (if tempo tempo *default-tempo*))))
+         (track (mapcan #'mo-event->midi events))
+         (midifile (make-instance 'midi:midifile
+                                  :format format
+                                  :division (* (/ *timebase* 4) *tick-multiplier*)
+                                  :tracks (list 
+                                           (cons tempo-msg
+                                                 (cons channel-msg track))))))
+    (midi:write-midi-file midifile file)
+    midifile))
+
+(defun mo-event->midi (event)
+  "Returns midi note on/off messages corresponding to the music object event <event>."
+  (let* ((non-onset (md:get-attribute event :onset))
+         (noff-onset (+ non-onset (md:get-attribute event :dur)))
+         (channel (md:get-attribute event :voice))
+         (keynum  (round (+ (- 60 *midc*)
+                            (md:get-attribute event :cpitch))))
+         (velocity (md:get-attribute event :dyn)))
+    (list (make-instance 'midi:note-on-message
+                         :time (* non-onset *tick-multiplier*)
+                         :status (+ #x90 channel)
+                         :key keynum 
+                         :velocity (if velocity velocity *default-velocity*))
+          (make-instance 'midi:note-off-message
+                         :time (* noff-onset *tick-multiplier*)
+                         :status (+ #x80 channel)
+                         :key keynum
+                         :velocity (if velocity velocity *default-velocity*)))))
+
+
+;;; Database events
+
+(defun db-event-lists->midi (event-lists file &key (format 1) (program *default-program*))
+  "Writes a list of lists of database events <event-lists> to a midi
+file <file> with each list of events written to a separate midi
+channel."
   (let* ((tempo (idyom-db:get-attribute (car (car event-lists)) :tempo))
          (tempo-msg (make-instance 'midi:tempo-message :time 0
                                    :status #xff
@@ -52,7 +114,7 @@
                                                                 :status (+ #xc0 channel) 
                                                                 :program program)))
                                (incf program)
-                               (cons channel-msg (cons tempo-msg (mapcan #'event->midi x))))) 
+                               (cons channel-msg (cons tempo-msg (mapcan #'db-event->midi x))))) 
                          event-lists))
          (midifile (make-instance 'midi:midifile
                                   :format format
@@ -60,7 +122,8 @@
                                   :tracks tracks)))
     (midi:write-midi-file midifile file)))
 
-(defun events->midi (events file &key (format 1) (program *default-program*))
+(defun db-events->midi (events file &key (format 1) (program *default-program*))
+  "Writes a list of database events <events> to a midi file <file>."
   (let* ((channel (idyom-db:get-attribute (car events) :voice))
          (channel-msg (make-instance 'midi:program-change-message :time 0 
                                      :status (+ #xc0 channel) 
@@ -70,7 +133,7 @@
                                    :status #xff
                                    :tempo (bpm->usecs 
                                            (if tempo tempo *default-tempo*))))
-         (track (mapcan #'event->midi events))
+         (track (mapcan #'db-event->midi events))
          (midifile (make-instance 'midi:midifile
                                   :format format
                                   :division (* (/ *timebase* 4) *tick-multiplier*)
@@ -80,7 +143,8 @@
     (midi:write-midi-file midifile file)
     midifile))
 
-(defun event->midi (event)
+(defun db-event->midi (event)
+  "Returns midi note on/off messages corresponding to the db event <event>."
   (let* ((non-onset (idyom-db:get-attribute event :onset))
          (noff-onset (+ non-onset (idyom-db:get-attribute event :dur)))
          (channel (idyom-db:get-attribute event :voice))
@@ -99,3 +163,7 @@
                          :velocity (if velocity velocity *default-velocity*)))))
   
 
+;;; Helper
+
+(defun bpm->usecs (bpm)
+  (floor (* 1000000 (/ 60 bpm))))
