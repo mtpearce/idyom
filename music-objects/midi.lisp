bf75ac66df97ad7034f7e40e78dca7e86f864934
Marcus Pearce <marcus.pearce@qmul.ac.uk>
2020-07-10 09:37:29 +0100

    Updates to midi export of music objects.

--- music-objects/midi.lisp
+++ music-objects/midi.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       midi.lisp
 ;;;; Author:     Marcus Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2020-02-05 07:01:51 marcusp>
-;;;; Time-stamp: <2020-05-18 16:09:24 marcusp>
+;;;; Time-stamp: <2020-07-10 09:33:42 marcusp>
 ;;;; ======================================================================
 
 (cl:in-package #:music-data)
@@ -19,6 +19,9 @@
 
 (defgeneric export-data (events type path &optional filename))
 
+(defmethod export-data ((mo music-object) (type (eql :mid)) path &optional filename)
+  (export-data (coerce mo 'list) type path filename))
+                                                                  
 (defmethod export-data ((event-list list) (type (eql :mid)) path &optional filename)
   "Export a list of music objects events (music-event)."
   (let* ((first-event (car event-list))
@@ -26,7 +29,7 @@
          (did (md:get-dataset-index id))
          (cid (md:get-composition-index id))
          (description (idyom-db:get-description did cid))
-         (filename (if filename filename (concatenate 'string path "/" description ".mid"))))
+         (filename (concatenate 'string path "/" (if filename filename description) ".mid")))
     (print filename)
     (mo-events->midi event-list filename)))
 
@@ -40,8 +43,10 @@
          (tempo (md:get-attribute (car events) :tempo))
          (keysig (md:get-attribute (car events) :keysig))
          (mode (md:get-attribute (car events) :mode))
+         (mode (when mode (if (= mode 9) 1 0)))
          (pulses (md:get-attribute (car events) :pulses))
          (barlength (md:get-attribute (car events) :barlength))
+         (midi-dd (when (and pulses barlength) (round (- (log (/ barlength (* pulses *timebase*)) 2)))))
          ;; channel
          (channel-msg (make-instance 'midi:program-change-message :time 0 
                                      :status (+ #xc0 channel) 
@@ -56,18 +61,25 @@
                                     :status #xff))
          ;; timesig
          (timesig-msg (make-instance 'midi:time-signature-message :time 0
-                                     :status #xff)))
-    (setf (slot-value keysig-msg 'midi::sf) keysig
-          (slot-value keysig-msg 'midi::mi) (if (= mode 9) 1 0)
-          (slot-value timesig-msg 'midi::nn) pulses
-          (slot-value timesig-msg 'midi::dd) (round (- (log (/ barlength (* pulses *timebase*)) 2)))
-          (slot-value timesig-msg 'midi::cc) 0
-          (slot-value timesig-msg 'midi::bb) 8)
-    (let* ((track (mapcan #'mo-event->midi events))
-           (midifile (make-instance 'midi:midifile
-                                    :format format
-                                    :division (* (/ *timebase* 4) *tick-multiplier*)
-                                    :tracks (list (append (list tempo-msg timesig-msg keysig-msg channel-msg) track)))))
+                                     :status #xff))
+         ;; midi track
+         (track (mapcan #'mo-event->midi events)))
+    (push channel-msg track)
+    (when (and keysig mode)
+      (setf (slot-value keysig-msg 'midi::sf) keysig
+            (slot-value keysig-msg 'midi::mi) mode)
+      (push keysig-msg track))
+    (when midi-dd
+      (setf (slot-value timesig-msg 'midi::nn) pulses
+            (slot-value timesig-msg 'midi::dd) midi-dd
+            (slot-value timesig-msg 'midi::cc) 0
+            (slot-value timesig-msg 'midi::bb) 8)
+      (push timesig-msg track))
+    (push tempo-msg track)
+    (let ((midifile (make-instance 'midi:midifile
+                                   :format format
+                                   :division (* (/ *timebase* 4) *tick-multiplier*)
+                                   :tracks (list track))))
       (midi:write-midi-file midifile file)
       midifile)))
 
