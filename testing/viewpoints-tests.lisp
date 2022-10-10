ccf6b7d2a64c8004d07d2d459931f89e96f878a2
Marcus Pearce <marcus.pearce@qmul.ac.uk>
2022-10-10 10:27:49 +0100

    Modify onset alphabet such that events are not predicted twice. This commit changes two aspects of the derivation of the onset alphabet: the alphabet given a null context corresponds to a length-one alphabet containing just the onset of the first event, and the alphabets for subsequent events no longer include the onset of the previous event itself (corresponding to an IOI of zero).

--- testing/viewpoints-tests.lisp
+++ testing/viewpoints-tests.lisp
@@ -2,7 +2,7 @@
 ;;;; File:       viewpoints-tests.lisp
 ;;;; Author:     Marcus Pearce <marcus.pearce@qmul.ac.uk>
 ;;;; Created:    <2022-06-23 13:17:12 peter>                         
-;;;; Time-stamp: <2022-07-06 12:58:36 marcusp>                           
+;;;; Time-stamp: <2022-10-10 10:01:34 marcusp>                           
 ;;;; ======================================================================
 ;;;;
 ;;;; Description ==========================================================
@@ -15,6 +15,110 @@
 (5am:def-suite viewpoints :in testing::idyom-tests)
 (5am:in-suite viewpoints)
 
+
+;;; Rhythm and timing (onset)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(5am:def-suite rhythm :in viewpoints :description "Unit tests for onset functions in extensions.lisp.")
+(5am:in-suite rhythm)
+
+(5am:def-fixture mock-rhythm-dataset (onset-sequences &optional (dataset-index 0))
+  (let* ((event-lists (mapcar (lambda (onsets)
+				(basic-attributes->music-events
+				 dataset-index 0 :onset onsets
+				 :bioi (cons (first onsets)
+					     (mapcar (lambda (a b) (- a b))
+						     (subseq onsets 1) onsets))))
+			      onset-sequences))
+	 ;; (music-sequences (mapcar (lambda (event-list composition-index)
+	 ;;        		    (make-instance 'md:music-sequence
+	 ;;        				   :id (md:make-composition-id dataset-index
+	 ;;        							       composition-index)
+	 ;;        				   :%data event-list))
+	 ;;        		  event-lists
+	 ;;        		  (generate-integers 0 (1- (length onset-sequences)))))
+	 ;; (dataset (make-instance 'md:music-dataset :id (md:make-dataset-id dataset-index)
+         ;;                         :%data music-sequences))
+         )
+    (&body)))
+
+(5am:test onset-alphabet-1 
+  (let* ((rhythms '((0 1) (1 3))))
+    (5am:with-fixture mock-rhythm-dataset (rhythms)
+      (initialise-basic-viewpoints event-lists)
+      (let ((onset (get-viewpoint 'onset)))
+	(5am:is (equal (viewpoint-alphabet onset) '(0 1 3)))))))
+
+(5am:test onset-alphabet-2
+  (let* ((rhythms '((0 1) (1 3))))
+    (5am:with-fixture mock-rhythm-dataset (rhythms)
+      (initialise-basic-viewpoints event-lists)
+      (let ((bioi (get-viewpoint 'bioi)))
+	(5am:is (equal (viewpoint-alphabet bioi) '(0 1 2)))))))
+
+(5am:test onset-alphabet-3 
+  (let* ((rhythms '((0 1) (1 3))))
+    (5am:with-fixture mock-rhythm-dataset (rhythms)
+      (initialise-basic-viewpoints event-lists)
+      (5am:is (equal (onset-alphabet (subseq (first event-lists) 0 1)) '(0))))))
+
+(5am:test onset-alphabet-4
+  (let* ((rhythms '((0 1) (1 3))))
+    (5am:with-fixture mock-rhythm-dataset (rhythms)
+      (initialise-basic-viewpoints event-lists)
+      (5am:is (equal (onset-alphabet (subseq (second event-lists) 0 1)) '(1))))))
+
+(5am:test onset-alphabet-5 
+  (let* ((rhythms '((0 1) (1 3))))
+    (5am:with-fixture mock-rhythm-dataset (rhythms)
+      (initialise-basic-viewpoints event-lists)
+      (5am:is (equal (onset-alphabet (first event-lists)) '(1 2))))))
+
+(5am:test onset-alphabet-6 
+  (let* ((rhythms '((0 1) (1 3))))
+    (5am:with-fixture mock-rhythm-dataset (rhythms)
+      (initialise-basic-viewpoints event-lists)
+      (5am:is (equal (onset-alphabet (second event-lists))'(2 3))))))
+
+(defun iois->durations (iois) (cdr iois))
+(defun iois->biois (iois) (butlast iois))
+(defun iois->onsets (iois) (apply #'utils:cumsum (iois->biois iois)))
+
+(defun list-pair->plist (keywords values)
+  (unless (or (null keywords) (null values))
+    (let ((k (car keywords))
+	  (v (car values)))
+      (cons k (cons v (list-pair->plist (cdr keywords) (cdr values)))))))
+
+;; (defparameter *default-attributes* (list-pair->plist (get-basic-attributes nil)
+;; 						     (loop repeat (length (get-basic-attributes nil))
+;; 							collect +undefined+)))
+
+(defun basic-attributes->music-events (dataset-index composition-index
+					  &rest basic-attribute-sequences)
+  "Given a PLIST, BASIC-ATTRIBUTE-SEQUENCES, generate a list of events whose basic
+attributes corresponds to the basic attributes in BASIC-ATTRIBUTE-SEQUENCES."
+  (let* ((basic-attributes (loop for i below (/ (length basic-attribute-sequences) 2)
+			      collect (nth (* i 2) basic-attribute-sequences)))
+	 (basic-attribute-sequences (loop for type in basic-attributes
+				       collect (getf basic-attribute-sequences type)))
+	 (event-indices (utils:generate-integers 0 (length (first basic-attribute-sequences)))))
+    (apply #'mapcar (cons (lambda (event-index &rest values)
+			    (let ((event (apply #'make-instance
+						(list 'md:music-event
+						      :id (md:make-event-id dataset-index
+									    composition-index
+									    event-index)))))
+			      (dolist (slot md::*music-slots* event)
+				(setf (slot-value event slot)
+				      (or (getf (list-pair->plist basic-attributes values)
+						(find-symbol (symbol-name slot) :keyword))
+					  +undefined+)))))
+			  (cons event-indices basic-attribute-sequences)))))
+
+
+;;; Harmony
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (5am:def-suite harmony :in viewpoints)
 (5am:in-suite harmony)
